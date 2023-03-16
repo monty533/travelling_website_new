10f7fd8939efac7ddd9dc3cbb668d62a775aa88a
nitin <monty4139@gmail.com>
2023-03-16 22:09:25 +0530

    custom user model and login and logout

---
+++ sartia_task/app/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import User
+
+# Register your models here.
+@admin.register(User)
+class CustomerModelAdmin(admin.ModelAdmin):
+    list_display = ['id', 'email', 'name',
+                    'locality', 'city', 'zipcode']
